052c8c895f21138715af9c59e8ee8fe8048c8e4c
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-03-18 12:46:40 -0700

    Updating Index View cshtml to work with new ViewModel definition.

--- src/CrudifiedMvc.View/Models/TestContext.cs
+++ src/CrudifiedMvc.View/Models/TestContext.cs
@@ -12,6 +12,11 @@ namespace CrudifiedMvc.View.Models
 {
     public class TestContext : Context
     {
+        public TestContext()
+        {
+            Database.SetInitializer<TestContext>(new CreateDatabaseIfNotExists<TestContext>());
+        }
+
         public IDbSet<CrudModel> CrudModels { get; set; }
         public IDbSet<RelatedModel> RelatedModels { get; set; }
         public IDbSet<RelatedManyModel> RelatedManyModels { get; set; }
