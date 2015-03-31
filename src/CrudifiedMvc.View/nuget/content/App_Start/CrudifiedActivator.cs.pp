33ef278f8441eb7672300f3c10ebd425d29a8f15
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-03-31 11:32:41 -0700

    Moving Api IndexController to ViewBuilding module.

--- src/CrudifiedMvc.View/nuget/content/App_Start/CrudifiedActivator.cs.pp
+++ src/CrudifiedMvc.View/nuget/content/App_Start/CrudifiedActivator.cs.pp
@@ -6,10 +6,7 @@ namespace $rootnamespace$.App_Start
     using System.Web.Optimization;
 	using System.Web.WebPages.Razor;
 
-    using System.Web.Http;
     using System.Web.Routing;
-
-    using Newtonsoft.Json.Serialization;
 
     using Crud = Psns.Common.Mvc.Crudified.Infrastructure;
     using Psns.Common.Mvc.ViewBuilding.Menu;
@@ -23,19 +20,12 @@ namespace $rootnamespace$.App_Start
         {
             Crud.RouteConfig.RegisterRoutes(RouteTable.Routes);
             Crud.BundleConfig.RegisterBundles(BundleTable.Bundles);
-            Crud.WebApiConfig.Register(GlobalConfiguration.Configuration);
 
             System.Web.Mvc.ControllerBuilder.Current.DefaultNamespaces.Add("Psns.Common.Mvc.Crudified.Controllers");
-			WebCodeRazorHost.AddGlobalImport("System.Web.Optimization");
+            WebCodeRazorHost.AddGlobalImport("System.Web.Optimization");
             WebCodeRazorHost.AddGlobalImport("Psns.Common.Mvc.ViewBuilding.Menu");
 
-            GlobalConfiguration.Configuration
-                .Formatters
-                .JsonFormatter
-                .SerializerSettings
-                .ContractResolver = new CamelCasePropertyNamesContractResolver();
-
-			GlobalMenu.ContextMenu = new Infrastructure.ContextMenu();
+            GlobalMenu.ContextMenu = new Infrastructure.ContextMenu();
         }
         
         /// <summary>
